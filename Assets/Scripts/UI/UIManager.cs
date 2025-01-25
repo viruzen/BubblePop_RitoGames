eb89573ef0cef783c50f329e76f88f349d072466
R-ut <126492912+R-ut@users.noreply.github.com>
2025-01-25 18:44:14 -0500

    Revert "HighScore Mechanism"
    This reverts commit a93436641eaa60fd67adebd97570cd56e3fd9c65.

--- Assets/Scripts/UI/UIManager.cs
+++ Assets/Scripts/UI/UIManager.cs
@@ -3,77 +3,55 @@ using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
 using System.IO;
-//namespace Scripts.UI;
-
-public class UIManager : MonoBehaviour
+namespace Scrpits.UI
 {
-    
-    [SerializeField] private TextMeshProUGUI scoreText;
-    public float Score;
-    [SerializeField] private TextMeshProUGUI highScoreText;
-    public float HighScore;
-
-    [SerializeField] private TMP_Text timerText;
-    [SerializeField] private Image slider;
-    [SerializeField] private float timeLimit = 60.0f;
-
-    
-
-    float time;
-    float multiplierFactor;
-
-    private void OnEnable()
+    public class UIManager : MonoBehaviour
     {
         
-    }
+        [SerializeField] private TextMeshProUGUI scoreText;
+        [SerializeField] private TextMeshProUGUI highScoreText;
 
-    private void Start()
-    {
-        if (GameManager.Instance != null)
+        [SerializeField] private TMP_Text timerText;
+        [SerializeField] private Image slider;
+        [SerializeField] private float timeLimit = 60.0f;
+
+
+        float time;
+        float multiplierFactor;
+
+        private void OnEnable()
         {
-            GameManager.Instance.OnScoreChanged.AddListener(UpdateScore); 
+            
         }
-        scoreText.text = "Score: 0";
 
-        multiplierFactor = 1.0f / timeLimit;
-        time = 60.0f;
-        slider.fillAmount = time * multiplierFactor;
+        private void Start()
+        {
+            if (GameManager.Instance != null)
+            {
+                GameManager.Instance.OnScoreChanged.AddListener(UpdateScore);
+            }
 
-        if (PlayerPrefs.HasKey("HighScore") )
-        {
-            HighScore = PlayerPrefs.GetFloat("HighScore");
-        }
-        else
-        {
-            HighScore = 0;
-        }
-    }
+            scoreText.text = "score: 0";
 
-    private void Update()
-    {
-        //HighScore = PlayerPrefs.GetFloat("HighScore");
-        if (time > 0)
-        {
-            time -= Time.deltaTime;
-            timerText.text = Mathf.CeilToInt(time).ToString();
+            multiplierFactor = 1.0f / timeLimit;
+            time = 60.0f;
             slider.fillAmount = time * multiplierFactor;
         }
-
-
-        if (PlayerPrefs.GetFloat("HighScore") < Score)
+        
+        private void Update()
         {
-            PlayerPrefs.SetFloat("HighScore", HighScore);
-            HighScore = Score;
-            highScoreText.text = "High Score: " + PlayerPrefs.GetFloat("HighScore");
+            if (time > 0)
+            {
+                time -= Time.deltaTime;
+                timerText.text = Mathf.CeilToInt(time).ToString();
+                slider.fillAmount = time * multiplierFactor;
+            }
         }
 
-        highScoreText.text = HighScore.ToString();
-        scoreText.text = Score.ToString();
-    }
-
-    private void UpdateScore(int newScore)
-    {
-        Score += newScore;
-        scoreText.text = "Score: "+newScore;
+        private void UpdateScore(int newScore)
+        {
+            scoreText.text = "Score: "+newScore;
+        }
+        
     }
 }
