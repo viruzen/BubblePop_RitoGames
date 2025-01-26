a924e9dbf6775ee52e1e3e19f540a5fbef2cdca8
R-ut <126492912+R-ut@users.noreply.github.com>
2025-01-25 22:47:06 -0500

    Highscore added

--- Assets/Scripts/UI/MainMenu.cs
+++ Assets/Scripts/UI/MainMenu.cs
@@ -1,11 +1,17 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
 public class MainMenu : MonoBehaviour
 {
+    [SerializeField] private TextMeshProUGUI highScoreText;
     public void StartGame(){
         SceneManager.LoadSceneAsync("Game");    
     }
 
+    private void Update()
+    {
+        highScoreText.text = "" + PlayerPrefs.GetFloat("HighScore");
+    }
     public void QuitGame()
     {
         Application.Quit();
